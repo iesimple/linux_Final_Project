17e451881a7c2c18f130ba3459ed8c36b1ad38e5
iesimple <2122190353@qq.com>
2022-06-03 16:19:31 +0800

    编译文件

---
+++ Hotel_system/build/src/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/os/桌面/linux_Final_Project/Hotel_system/src/isbadptr.c" "/home/os/桌面/linux_Final_Project/Hotel_system/build/src/CMakeFiles/main.dir/isbadptr.c.o"
+  "/home/os/桌面/linux_Final_Project/Hotel_system/src/main.c" "/home/os/桌面/linux_Final_Project/Hotel_system/build/src/CMakeFiles/main.dir/main.c.o"
+  "/home/os/桌面/linux_Final_Project/Hotel_system/src/process.c" "/home/os/桌面/linux_Final_Project/Hotel_system/build/src/CMakeFiles/main.dir/process.c.o"
+  "/home/os/桌面/linux_Final_Project/Hotel_system/src/systemInit.c" "/home/os/桌面/linux_Final_Project/Hotel_system/build/src/CMakeFiles/main.dir/systemInit.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../src/../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
